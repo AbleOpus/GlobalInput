27539b5b7abc73084fbe1c69e3fba0e0f84279e1
Brian Ferguson <brianferguson@hotmail.ca>
2016-06-06 02:57:09 -0300

    XML Adjustment.

--- GlobalInput/Keyboard/ExtraKeys.cs
+++ GlobalInput/Keyboard/ExtraKeys.cs
@@ -7,7 +7,9 @@ namespace GlobalInput.Keyboard
         /// <summary>
         /// Gets the Windows key modifier.
         /// </summary>
-        /// <remarks>This flag comes immediately after the Alt flag.</remarks>
+        /// <remarks>This flag comes immediately after the Alt flag. This flag is not used
+        /// by anything but <see cref="GlobalInput"/>.
+        /// </remarks>
         internal const Keys WinKeyModifier = (Keys)524288;
     }
 }
