9df5953d2b3b3be0503d018403b7bc0b9c7ec33d
Brian Ferguson <brianferguson@hotmail.ca>
2016-06-06 02:39:32 -0300

    Reduced accessibility of the Modifiers enum.

--- GlobalInput/Keyboard/HotkeyHooker.cs
+++ GlobalInput/Keyboard/HotkeyHooker.cs
@@ -18,6 +18,34 @@ namespace GlobalInput.Keyboard
     public class HotkeyHooker : IDisposable, INotifyPropertyChanged
     {
         /// <summary>
+        /// Specifies the unmanaged representation of modifier keys.
+        /// </summary>
+        [Flags]
+        private enum Modifiers
+        {
+            /// <summary>
+            /// No modifiers specified.
+            /// </summary>
+            None = 0x0000,
+            /// <summary>
+            /// The alt modifier key.
+            /// </summary>
+            Alt = 0x0001,
+            /// <summary>
+            /// The control modifier key.
+            /// </summary>
+            Control = 0x0002,
+            /// <summary>
+            /// The shift modifier key.
+            /// </summary>
+            Shift = 0x0004,
+            /// <summary>
+            /// The Windows key.
+            /// </summary>
+            Win = 0x0008
+        }
+
+        /// <summary>
         /// Represents a window for receiving hotkey messages.
         /// </summary>
         private sealed class HotkeyBindingWindow : NativeWindow, IDisposable
